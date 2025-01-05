3cbf195033fddf5d592bc823a3d44dda36b72f95
Laura Hoehn <laura02hoehngmail.com>
2025-01-05 11:51:25 +0100

    Erstes Konzept des Logins

---
+++ src/components/LoginView.vue
@@ -0,0 +1,211 @@
+<script>
+//import { supabase } from '@/Clients/supabaseClient';
+
+import { SessionManager } from '@/Clients/sessionManager'; //  Session-Handling
+
+
+  export default {
+    data: () => {
+      return {
+      email: "",
+      password: "",
+      errorMessage: "", // Fehlermeldungen anzeigen
+      }
+    },
+
+   // Hier wird der Benutzername und das Passwort von der Registrierung übernommen
+
+    created() {
+        this.email = this.$route.query.email || '';
+        this.password = this.$route.query.password || '';
+      },
+
+
+    methods: {
+
+    //nicht final, nur ein 'Platzhalter' für die Weiterleitung zur Bewertungskarte
+      goToRatingCard(){
+          this.$router.push({ name: 'Bewertung'})
+        },
+
+      async loginUser() {
+        try {
+          const { data, error } = await SessionManager.signIn(this.email, this.password);
+
+
+          if(error) {
+            this.errorMessage = "Login fehlgeschlagen: " + error.message;
+            return;
+
+          }
+          console.log("Login erfolgreich: ", data);
+
+
+          // Prüfe, ob die Session erfolgreich gesetzt wurde
+        const session = await SessionManager.getCurrentSession();
+        if (!session) {
+          this.errorMessage = 'Sitzung konnte nicht gestartet werden.';
+          return;
+        }
+
+          //Nach erfolgreichem Login weiter zur Home-Ansicht
+          this.$router.push({name: 'Fahrt'});
+        } catch(err) {
+          this.errorMessage = "Ein unerwarteter Fehler ist aufgetreten.";
+          console.error("Login-Fehler: ", err);
+        }
+      },
+    },
+  };
+
+</script>
+
+<style scoped>
+
+
+.icon {
+display: flex;
+justify-content: center;
+margin-bottom: -100px;
+
+}
+.container {
+
+  display: flex;
+  justify-content: center;
+  margin:auto
+}
+
+.container .icon {
+
+ display: flex;
+ margin: auto;
+ justify-content: center;
+
+}
+
+.header h1 {
+
+  display: flex;
+  padding-bottom: 20px ;
+  justify-content: center;
+  font-weight:500;
+
+}
+
+.header h4 {
+
+  display: flex;
+  justify-content: center;
+  color: rgb(54, 70, 45);
+  padding-bottom: 50px ;
+  font-style: italic;
+  font-weight: 200;
+  font-weight: 300;
+  font-size: larger;
+}
+
+.textfield {
+
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 500px;
+
+}
+
+.button {
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin:auto;
+  margin-top: 50px;
+  padding: 30px;
+  background-color: rgba(47, 96, 36, 0.274);
+}
+
+.question {
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  margin-top: 30px;
+
+
+}
+
+.link  {
+  color:rgba(47, 96, 36, 0.613);
+  text-decoration-line:none ;
+  text-decoration-color: none;
+  margin-left:3px ;
+}
+</style>
+
+<template>
+    <v-form @submit.prevent="loginUser">
+      <div class="icon">
+        <v-avatar
+          size="400"
+        >
+        <!-- Pfad zum Logo -->
+          <v-img
+            class="image"
+            alt="Logo"
+            src="../assets/CC_logo.png" 
+          />
+        </v-avatar>
+      </div>
+      <div class="container">
+        <container>
+          <header class="header">
+            <h1>Willkommen zurück!</h1>
+  
+            <h4>melde dich an und fahre los</h4>
+          </header>
+          
+          <div class="textfield">
+            <v-text-field
+              id="email"
+              v-model="email"
+              label="Deine Email"
+              placeholder="Gebe deine Uni-Mail (uni-bayreuth/myubt.de) an."
+              variant="solo-filled"
+              rounded
+              prepend-inner-icon="mdi-email-outline"
+            />
+            <v-text-field
+              id="password"
+              v-model="password"
+              label="Passwort"
+              variant="solo-filled"
+              rounded
+              prepend-inner-icon="mdi-lock-outline"
+            />
+          </div>
+          <v-btn
+            class="button"
+            type="submit"
+            rounded="xl"
+            size="x-large"
+            @click="loginUser"
+          >
+            Einloggen
+          </v-btn>
+          <div class="question">
+          <!-- Link zur Registrierung -->
+            <p>
+              Du hast noch keinen einen Account?
+              <router-link
+                class="link"
+                :to="{name: 'Registrierung'}"
+                @click="gotToRegister"
+              >
+                Registrieren
+              </router-link>
+            </p>
+          </div>
+        </container>
+      </div>
+    </v-form>
+  </template>

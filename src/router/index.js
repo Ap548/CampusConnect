4e4fc5afe1865e30af70891df25808a131170eb9
Laura Hoehn <laura02hoehngmail.com>
2025-01-16 16:49:47 +0100

    Hinzufügen des Routers

---
+++ src/router/index.js
@@ -0,0 +1,88 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import FahrerProfil from '@/components/FahrerProfil.vue'
+import LoginView from '@/components/LoginView.vue'
+import RegisterView from '@/components/RegisterView.vue'
+import BewertungView from '@/components/BewertungView.vue'
+import AddFahrtView from '@/components/AddFahrtView.vue'
+
+//import { components } from 'vuetify/dist/vuetify-labs.js'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [ 
+
+
+    // {
+    //   path: '/home',
+    //   name: 'Home',
+    //   component: Home
+
+    // },
+
+    {
+      path: '/registrierung',
+      name: 'Registrierung',
+      component: RegisterView
+    },
+
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+    },
+
+    {
+      path:'/bewertung',
+      name:'Bewertung',
+      component: BewertungView
+
+    },
+
+    {
+        path:'/fahrt',
+        name: 'Fahrt',
+        component: AddFahrtView
+    },
+
+     {
+      path: '/profil',
+      name: 'Profil',
+      component: FahrerProfil
+     },
+
+    
+
+     
+    ],
+
+
+})
+
+
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+    if (!localStorage.getItem('vuetify:dynamic-reload')) {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    } else {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
+})
+
+export default router

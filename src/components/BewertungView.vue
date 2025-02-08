4dbe3bfd585de5975222c0da4cafaf17698064bc
Ap548 <Pascal-Albert@gmx.de>
2025-02-08 20:06:34 +0100

    Schließen Button von Bewertung entfernt

--- src/components/BewertungView.vue
+++ src/components/BewertungView.vue
@@ -98,12 +98,6 @@
       class="mx-auto"
       width="400"
     >
-
-  <!--neu-->
-  <button class="Schließen" @click="ToggleBewertungView()">Schließen</button>
-
-  
-
       <v-card-title class="text-h5">
         Bewerte deine Fahrt
       </v-card-title>
